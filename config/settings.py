7270e21316aec3bdca16705030afaf2977d60f58
LAPTOP-CISI8I61\user <kerneltrek@gmail.com>
2022-06-23 12:07:05 +0900

    input secret key comment. because This is secret contents

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Input setting.
 secret_file = os.path.join(BASE_DIR, 'secret.json')
 
 with open(secret_file) as f:
